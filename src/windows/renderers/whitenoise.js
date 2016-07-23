07328dbf5ab08ce08e284301a1f17410f297441b
Phantr4x <phantr4x@gmail.com>
2016-07-23 17:38:26 +0800

    v0.4.0 1. 白噪音功能完成，现在开始享受工作乐趣吧(=σωσ=)♪

--- src/windows/renderers/whitenoise.js
+++ src/windows/renderers/whitenoise.js
@@ -1,11 +1,31 @@
 'use strict';
 
 (() => {
-  var item = document.getElementsByClassName('whitenoise-item');
+  var item = document.querySelectorAll('.whitenoise-playpause');
   // var icon = document.querySelectorAll('.whitenoise-icon')[0];
-  item.onclick = (event) => {
-    console.log(this);
-    this.getElementsByClassName('.whitenoise-audioplayer')[0].getElementsByTagName('audio').play();
-  };
+  item.forEach((element, index, array) => {
+    // console.log(element);
+    element.onclick = () => {
+      // console.log(element.parentNode.childNodes[1]);
+      element.getAttribute('data-switch') === 'true'
+        ? element.setAttribute('data-switch', 'false')|| element.parentNode.parentNode.classList.remove('whitenoise-active') || element.parentNode.childNodes[1].pause()
+        : element.setAttribute('data-switch', 'true') || element.parentNode.parentNode.classList.add('whitenoise-active') || element.parentNode.childNodes[1].play();
+      // console.log(element.getAttribute('data-switch'));
+    }
+  });
 
+  var scrollLeft = document.getElementById('whitenoise-scroll-left'),
+    scrollRight = document.getElementById('whitenoise-scroll-right');
+  scrollRight.onclick = (event) => {
+    event.preventDefault();
+    scrollRight.style.display = 'none';
+    scrollLeft.style.display = 'inline-block';
+    document.querySelector('.whitenoise-carousel-slides').style.left = '-300px';
+  }
+  scrollLeft.onclick = (event) => {
+    event.preventDefault();
+    scrollLeft.style.display = 'none';
+    scrollRight.style.display = 'inline-block';
+    document.querySelector('.whitenoise-carousel-slides').style.left = '0px';
+  }
 })();
